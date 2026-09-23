71d6e0cc5dff8a24dc0794622f727aeb1352a1a7
proumezi-creator <proumezi@gmail.com>
2026-09-23 16:41:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base: '/ops-supply-questionnaire/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
